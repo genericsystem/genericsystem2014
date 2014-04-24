0d890a05fba5b118934ead3fdf7e5bbd71e29281
nfeybesse <nfeybesse@middlewarefactory.com>
2014-04-24 19:35:22 +0200

    Merge

--- genericsystem-kernel/src/main/java/org/genericsystem/kernel/services/DependenciesService.java
+++ genericsystem-kernel/src/main/java/org/genericsystem/kernel/services/DependenciesService.java
@@ -1,23 +1,26 @@
 package org.genericsystem.kernel.services;
 
+import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map.Entry;
+import org.genericsystem.kernel.DependenciesImpl;
 import org.genericsystem.kernel.Snapshot;
 import org.genericsystem.kernel.Snapshot.AbstractSnapshot;
 import org.genericsystem.kernel.Vertex;
 import org.genericsystem.kernel.exceptions.ExistException;
 import org.genericsystem.kernel.exceptions.NotFoundException;
 
-public interface DependenciesService extends AncestorsService, FactoryService {
+public interface DependenciesService extends AncestorsService {
 
-	public static abstract class Dependencies extends AbstractSnapshot<Vertex> {
+	static abstract class Dependencies<T> extends AbstractSnapshot<T> {
 
-		abstract protected boolean remove(Vertex vertex);
+		abstract protected boolean remove(T vertex);
 
-		abstract protected void add(Vertex vertex);
+		abstract protected void add(T vertex);
 
-		public Vertex set(Vertex vertex) {
-			Vertex result = get(vertex);
+		public T set(T vertex) {
+			T result = get(vertex);
 			if (result == null) {
 				add(vertex);
 				return vertex;
@@ -26,29 +29,53 @@ public interface DependenciesService extends AncestorsService, FactoryService {
 		}
 	}
 
-	public static abstract class CompositesDependencies extends AbstractSnapshot<Entry<Vertex, Dependencies>> {
-
-		abstract protected boolean remove(Vertex vertex);
-
-		abstract protected void add(Vertex vertex);
-
-		public Vertex set(Vertex vertex) {
-			Vertex result = get(vertex);
-			if (result == null) {
-				add(vertex);
-				return vertex;
+	class CompositesDependencies<T> extends DependenciesImpl<Entry<T, Dependencies<T>>> {
+		private Dependencies<T> internalGetByIndex(T index) {
+			Iterator<Entry<T, Dependencies<T>>> it = iterator();
+			while (it.hasNext()) {
+				Entry<T, Dependencies<T>> next = it.next();
+				if (index.equals(next.getKey()))
+					return next.getValue();
 			}
-			return result;
+			return null;
+		}
+
+		public Snapshot<T> getByIndex(T index) {
+			Snapshot<T> result = internalGetByIndex(index);
+			return result != null ? result : AbstractSnapshot.<T> emptySnapshot();
+		}
+
+		public T setByIndex(T index, T vertex) {
+			Dependencies<T> result = internalGetByIndex(index);
+			if (result == null) {
+				result = ((Vertex) DependenciesService.this).getFactory().<T> buildDependency((Vertex) index);
+				super.set(new AbstractMap.SimpleEntry<T, Dependencies<T>>(index, result));
+			}
+			return result.set(vertex);
+		}
+
+		public T removeByIndex(T index, T vertex) {
+			// Dependencies<Vertex> result = internalGetByIndex(index);
+			// if (result == null) {
+			// result = DependenciesService.this.getFactory().buildDependency(index);
+			// super.set(new AbstractMap.SimpleEntry<Vertex, Dependencies<Vertex>>(index, result));
+			// }
+			// return result.set(vertex);
+			return null;
 		}
 	}
 
-	Dependencies getInstances();
+	Snapshot<Vertex> getInstances();
 
-	Dependencies getInheritings();
+	Snapshot<Vertex> getInheritings();
 
-	default Snapshot<Vertex> getMetaComposites(Vertex meta);
+	Snapshot<Vertex> getMetaComposites(Vertex meta);
 
-	Snapshot<Vertex> getSuperComposites(Vertex superVertex);
+	Snapshot<?> getMetaComposites();
+
+	Snapshot<?> getSuperComposites(Vertex superVertex);
+
+	Snapshot<?> getSuperComposites();
 
 	default boolean isPlugged() throws NotFoundException {
 		return this == getPlugged();
@@ -66,7 +93,8 @@ public interface DependenciesService extends AncestorsService, FactoryService {
 			return vertex;
 		}
 		getSupersStream().forEach(superGeneric -> superGeneric.getInheritings().set((Vertex) this));
-		Arrays.asList(getComponents()).forEach(component -> component.getComposites().set((Vertex) this));
+		Arrays.asList(getComponents()).forEach(component -> component.getMetaComposites().setByIndex(getMeta(), (Vertex) this));
+		getSupersStream().forEach(superGeneric -> Arrays.asList(getComponents()).forEach(component -> component.getSuperComposites().setByIndex(superGeneric, (Vertex) this)));
 		return vertex;
 	}
 
@@ -75,7 +103,8 @@ public interface DependenciesService extends AncestorsService, FactoryService {
 		if (!result)
 			throw new NotFoundException((Vertex) this);
 		getSupersStream().forEach(superGeneric -> superGeneric.getInheritings().remove((Vertex) this));
-		Arrays.asList(getComponents()).forEach(component -> component.getComposites().remove((Vertex) this));
+		Arrays.asList(getComponents()).forEach(component -> component.getMetaComposites().removeByIndex(getMeta(), (Vertex) this));
+		getSupersStream().forEach(superGeneric -> Arrays.asList(getComponents()).forEach(component -> component.getSuperComposites().removeByIndex(superGeneric, (Vertex) this)));
 		return result;
 	}
 }
