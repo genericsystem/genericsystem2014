365110646ade490f05e3f09db37563b4d78e56d1
mory <mory@middlewarefactory.com>
2014-05-13 17:05:59 +0200

    fix cache

--- gs-cache/src/main/java/org/genericsystem/cache/Cache.java
+++ gs-cache/src/main/java/org/genericsystem/cache/Cache.java
@@ -1,9 +1,5 @@
 package org.genericsystem.cache;
 
-<<<<<<< HEAD
-public interface Cache {
-=======
 public class Cache {
->>>>>>> branch 'master' of https://github.com/genericsystem/genericsystem2014.git
 
 }
