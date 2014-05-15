7f33cb8b43d02764e0da523e9fed76d164e6128d
nfeybesse <nfeybesse@middlewarefactory.com>
2014-05-15 17:12:17 +0200

    Fix issues in getInheritings and cache

--- gs-cache/src/main/java/org/genericsystem/cache/Cache.java
+++ gs-cache/src/main/java/org/genericsystem/cache/Cache.java
@@ -1,89 +1,23 @@
 package org.genericsystem.cache;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import org.genericsystem.impl.GenericService;
 import org.genericsystem.kernel.Dependencies;
-import org.genericsystem.kernel.DependenciesImpl;
-import org.genericsystem.kernel.iterator.AbstractAwareIterator;
 
 public class Cache<T extends GenericService<T>> {
 
-	private transient Map<GenericService<T>, CacheDependencies<T>> inheritingDependenciesMap = new HashMap<GenericService<T>, Cache.CacheDependencies<T>>();
+	private transient Map<T, Dependencies<T>> inheritingDependenciesMap = new HashMap<T, Dependencies<T>>();
+	private transient Map<T, Dependencies<T>> instancesDependenciesMap = new HashMap<T, Dependencies<T>>();
+	private transient Map<T, Dependencies<T>> metaCompositesDependenciesMap = new HashMap<T, Dependencies<T>>();
+	private transient Map<T, Dependencies<T>> superCompositesDependenciesMap = new HashMap<T, Dependencies<T>>();
 
-	public CacheDependencies<T> getInheritingDependencies(GenericService<T> generic) {
-		CacheDependencies<T> dependencies = getInheritingDependencies(generic);
-		if (dependencies == null) {
-			Dependencies<T> result = inheritingDependenciesMap.put(generic, dependencies = new CacheDependencies<T>(generic.getAlive().getInheritings().project(generic::wrap, GenericService::unwrap)));
-			assert result == null;
-		}
-		return dependencies;
+	Map<T, Dependencies<T>> getInheritingDependenciesMap() {
+		return inheritingDependenciesMap;
 	}
 
-	static class CacheDependencies<T> implements Dependencies<T> {
-
-		// TODO : underlyingDependencies make a cache here
-		// TODO : underlyingDependencies iterator should be gettable on demand !
-		private transient Dependencies<T> underlyingDependencies;
-
-		private final DependenciesImpl<T> inserts = new DependenciesImpl<T>();
-		private final DependenciesImpl<T> deletes = new DependenciesImpl<T>();
-
-		public CacheDependencies(Dependencies<T> underlyingDependencies) {
-			assert underlyingDependencies != null;
-			this.underlyingDependencies = underlyingDependencies;
-		}
-
-		@Override
-		public void add(T generic) {
-			inserts.add(generic);
-		}
-
-		@Override
-		public boolean remove(T generic) {
-			if (!inserts.remove(generic)) {
-				deletes.add(generic);
-				return true;
-			}
-			return false;
-		}
-
-		@Override
-		public Iterator<T> iterator() {
-			return new InternalIterator(underlyingDependencies.iterator());
-		}
-
-		// TODO This implementation is inefficient !
-		private class InternalIterator extends AbstractAwareIterator<T> implements Iterator<T> {
-			private final Iterator<T> underlyingIterator;
-			private final Iterator<T> insertsIterator = inserts.iterator();
-
-			private InternalIterator(Iterator<T> underlyingIterator) {
-				this.underlyingIterator = underlyingIterator;
-			}
-
-			@Override
-			protected void advance() {
-				while (underlyingIterator.hasNext()) {
-					T generic = underlyingIterator.next();
-					if (!deletes.contains(generic)) {
-						next = generic;
-						return;
-					}
-				}
-				while (insertsIterator.hasNext()) {
-					next = insertsIterator.next();
-					return;
-				}
-				next = null;
-			}
-
-			@Override
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		}
+	Map<T, Dependencies<T>> getInstancesDependenciesMap() {
+		return instancesDependenciesMap;
 	}
 
 }
--- gs-cache/src/main/java/org/genericsystem/cache/GenericService.java
+++ gs-cache/src/main/java/org/genericsystem/cache/GenericService.java
@@ -1,6 +1,11 @@
 package org.genericsystem.cache;
 
+import java.util.Iterator;
+import java.util.Map;
 import org.genericsystem.kernel.Dependencies;
+import org.genericsystem.kernel.DependenciesImpl;
+import org.genericsystem.kernel.iterator.AbstractConcateIterator.ConcateIterator;
+import org.genericsystem.kernel.iterator.AbstractFilterIterator;
 
 public interface GenericService<T extends GenericService<T>> extends org.genericsystem.impl.GenericService<T> {
 
@@ -8,8 +13,46 @@ public interface GenericService<T extends GenericService<T>> extends org.generic
 		return getMeta().getCurrentCache();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	default Dependencies<T> getInheritings() {
-		return getCurrentCache().getInheritingDependencies(this);
+		Map<T, Dependencies<T>> inheritingDependenciesMap = getCurrentCache().getInheritingDependenciesMap();
+		Dependencies<T> dependencies = inheritingDependenciesMap.get(GenericService.this);
+		if (dependencies == null) {
+			Iterator<T> inheritingsIterator = org.genericsystem.impl.GenericService.super.getInheritings().iterator();
+
+			class CacheDependencies implements Dependencies<T> {
+
+				private final DependenciesImpl<T> inserts = new DependenciesImpl<T>();
+				private final DependenciesImpl<T> deletes = new DependenciesImpl<T>();
+
+				@Override
+				public void add(T generic) {
+					inserts.add(generic);
+				}
+
+				@Override
+				public boolean remove(T generic) {
+
+					if (!inserts.remove(generic)) {
+						deletes.add(generic);
+						return true;
+					}
+					return false;
+				}
+
+				@Override
+				public Iterator<T> iterator() {
+					return new ConcateIterator<T>(new AbstractFilterIterator<T>(inheritingsIterator) {
+						@Override
+						public boolean isSelected() {
+							return !deletes.contains(next);
+						}
+					}, inserts.iterator());
+				}
+			}
+			inheritingDependenciesMap.put((T) GenericService.this, dependencies = new CacheDependencies());
+		}
+		return dependencies;
 	}
 }
