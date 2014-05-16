2c4230fdc5bd19d10888e5944189aba11c45bd61
nfeybesse <nfeybesse@middlewarefactory.com>
2014-05-16 10:48:53 +0200

    New solution

--- gs-cache/src/main/java/org/genericsystem/cache/GenericService.java
+++ gs-cache/src/main/java/org/genericsystem/cache/GenericService.java
@@ -1,7 +1,11 @@
 package org.genericsystem.cache;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.function.Function;
 import org.genericsystem.kernel.Dependencies;
 import org.genericsystem.kernel.Dependencies.CompositesDependencies;
+import org.genericsystem.kernel.Vertex;
 
 public interface GenericService<T extends GenericService<T>> extends org.genericsystem.impl.GenericService<T> {
 
@@ -12,7 +16,12 @@ public interface GenericService<T extends GenericService<T>> extends org.generic
 	@SuppressWarnings("unchecked")
 	@Override
 	default Dependencies<T> getInheritings() {
-		return getCurrentCache().getInheritings((T) this, () -> org.genericsystem.impl.GenericService.super.getInheritings().iterator());
+		return getCurrentCache().getInheritings((T) this, () -> iteratorFromAlive(Vertex::getInheritings));
+	}
+
+	default Iterator<T> iteratorFromAlive(Function<Vertex, Dependencies<Vertex>> dependencies) {
+		Vertex alive = getAlive();
+		return alive == null ? Collections.emptyIterator() : dependencies.apply(alive).project(this::wrap).iterator();
 	}
 
 	@SuppressWarnings("unchecked")
