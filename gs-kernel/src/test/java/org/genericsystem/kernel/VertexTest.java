d8ca83dc6e5ea4f6f334ea29a878d131c74d8747
nfeybesse <nfeybesse@middlewarefactory.com>
2014-05-20 14:03:04 +0200

    Check isAlive in test16

--- gs-kernel/src/test/java/org/genericsystem/kernel/VertexTest.java
+++ gs-kernel/src/test/java/org/genericsystem/kernel/VertexTest.java
@@ -276,6 +276,8 @@ public class VertexTest extends AbstractTest {
 		assert vehiclePower.isAncestorOf(v233);
 		assert vehicle.isAncestorOf(v233);
 		Vertex car233 = vehiclePower.build().initFromOverrides(vehiclePower, Collections.emptyList(), 233, Arrays.asList(car));
+		assert !car233.isAlive();
+		assert v233.isAlive();
 		assert car233.isAncestorOf(v233);
 		assert car233.computeAllDependencies().contains(v233);
 	}
