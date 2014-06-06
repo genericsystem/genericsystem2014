df3da3e1ee680ddb4c0e589663cf46f402749fe0
Mohamed Khalil El Abed <kelabed@middlewarefactory.com>
2014-06-06 15:32:02 +0200

    Setup of GS-CDI for GenericSystem 2014

---
+++ gs-cdi/src/main/java/org/genericsystem/cdi/CacheProvider.java
@@ -0,0 +1,49 @@
+package org.genericsystem.cdi;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+import org.genericsystem.core.Cache;
+import org.genericsystem.core.Engine;
+
+@SessionScoped
+public class CacheProvider implements Serializable {
+
+	private static final long serialVersionUID = 5201003234496546928L;
+
+	@Inject
+	private transient Engine engine;
+
+	private Cache currentCache;
+
+	@PostConstruct
+	public void init() {
+		currentCache = engine.newCache();
+	}
+
+	public void mountNewCache() {
+		currentCache = currentCache.mountNewCache();
+	}
+
+	public void flushCurrentCache() {
+		currentCache = currentCache.flushAndUnmount();
+	}
+
+	public void discardCurrentCache() {
+		currentCache = currentCache.discardAndUnmount();
+	}
+
+	@Produces
+	public Cache getCurrentCache() {
+		return currentCache;
+	}
+
+	public void setCurrentCache(Cache cache) {
+		this.currentCache = cache;
+	}
+
+}
---
+++ gs-cdi/src/main/java/org/genericsystem/cdi/SerializableCache.java
@@ -0,0 +1,151 @@
+package org.genericsystem.cdi;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.inject.Vetoed;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+
+import org.genericsystem.core.AbstractContext;
+import org.genericsystem.core.AbstractWriter;
+import org.genericsystem.core.AbstractWriter.AbstractLoader;
+import org.genericsystem.core.Cache;
+import org.genericsystem.core.CacheImpl;
+import org.genericsystem.core.Engine;
+import org.genericsystem.core.Generic;
+import org.genericsystem.core.GenericImpl;
+import org.genericsystem.core.HomeTreeNode;
+import org.genericsystem.core.Transaction;
+
+@Vetoed
+public class SerializableCache extends CacheImpl implements Externalizable {
+
+	public SerializableCache() {
+		super((Cache) null);
+	}
+
+	public SerializableCache(Cache cache) {
+		super(cache);
+	}
+
+	public SerializableCache(Engine engine) {
+		super(engine);
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		new SerializableWriter(out).writeGenericsAndFlush();
+	}
+
+	private class SerializableWriter extends AbstractWriter {
+		private ObjectOutput out;
+
+		public SerializableWriter(ObjectOutput out) {
+			this.out = out;
+		}
+
+		@Override
+		public void writeGenerics() throws IOException {
+			if (subContext instanceof CacheImpl) {
+				out.writeBoolean(true);
+				out.writeObject(subContext);
+			} else {
+				out.writeBoolean(false);
+				out.writeLong(((Transaction) subContext).getTs());
+			}
+			Map<Long, HomeTreeNode> homeTreeMap = new HashMap<>();
+			out.writeInt(adds.size());
+			for (Generic add : adds)
+				writeGeneric((GenericImpl) add, homeTreeMap);
+			out.writeInt(automatics.size());
+			for (Generic automatic : automatics)
+				writeGeneric((GenericImpl) automatic, homeTreeMap);
+			out.writeInt(removes.size());
+			for (Generic remove : removes)
+				out.writeLong(((GenericImpl) remove).getDesignTs());
+		}
+
+		@Override
+		public ObjectOutputStream getFormalOutputStream() {
+			return (ObjectOutputStream) out;
+		}
+
+		@Override
+		public ObjectOutputStream getContentOutputStream() {
+			return getFormalOutputStream();
+		}
+
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+		Engine engine = StartupBean.getBean(Engine.class, getBeanManager());
+		Cache currentCache = engine.getCurrentCache();
+		try {
+			subContext = in.readBoolean() ? (AbstractContext) in.readObject() : new Transaction(in.readLong(), engine);
+			start();
+			Map<Long, HomeTreeNode> homeTreeMap = new HashMap<>();
+			Map<Long, Generic> genericMap = new HashMap<>();
+			int addSize = in.readInt();
+			SerializableLoader loader = new SerializableLoader(in);
+			for (int i = 0; i < addSize; i++)
+				adds.add(loader.loadGeneric(engine, homeTreeMap, genericMap));
+			int automaticSize = in.readInt();
+			for (int i = 0; i < automaticSize; i++)
+				automatics.add(loader.loadGeneric(engine, homeTreeMap, genericMap));
+			int removeSize = in.readInt();
+			for (int i = 0; i < removeSize; i++)
+				removes.add(findByDesignTs(engine, in.readLong(), genericMap));
+		} finally {
+			currentCache.start();
+		}
+	}
+
+	protected BeanManager getBeanManager() {
+		BeanManager beanManager = null;
+		try {
+			beanManager = CDI.current().getBeanManager();
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		if (beanManager == null)
+			throw new IllegalStateException();
+		return beanManager;
+	}
+
+	private static class SerializableLoader extends AbstractLoader {
+		private ObjectInput in;
+
+		public SerializableLoader(ObjectInput in) {
+			this.in = in;
+		}
+
+		@Override
+		public ObjectInputStream getContentInputStream() {
+			return getFormalInputStream();
+		}
+
+		@Override
+		public ObjectInputStream getFormalInputStream() {
+			return (ObjectInputStream) in;
+		}
+
+		@Override
+		protected void plug(GenericImpl generic) {
+			generic.getCurrentCache().plug(generic);
+		}
+
+		@Override
+		protected Generic loadAncestor(Engine engine, long ts, Map<Long, Generic> genericMap) {
+			return ((CacheImpl) engine.getCurrentCache()).findByDesignTs(engine, ts, genericMap);
+		}
+	}
+
+}
