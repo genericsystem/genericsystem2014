ab00a24b2c74d556a81bf88c59344d00d2c7e814
nfeybesse <nfeybesse@middlewarefactory.com>
2014-06-19 11:39:23 +0200

    Merge

--- gs-impl/src/test/java/org/genercisystem/impl/Generic.java
+++ gs-impl/src/test/java/org/genercisystem/impl/Generic.java
@@ -3,7 +3,6 @@ package org.genercisystem.impl;
 import org.genericsystem.impl.GenericService;
 import org.genericsystem.impl.GenericSignature;
 
-
 public class Generic extends GenericSignature<Generic> implements GenericService<Generic> {
 
 	@Override
