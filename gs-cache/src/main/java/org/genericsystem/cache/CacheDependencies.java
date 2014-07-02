e07ee3d4182c37790d8e3ae0d49a80269c6a4929
Mohamed Khalil El Abed <kelabed@middlewarefactory.com>
2014-07-02 11:08:01 +0200

    Merge

--- gs-cache/src/main/java/org/genericsystem/cache/CacheDependencies.java
+++ gs-cache/src/main/java/org/genericsystem/cache/CacheDependencies.java
@@ -2,6 +2,7 @@ package org.genericsystem.cache;
 
 import java.util.Iterator;
 import java.util.function.Supplier;
+
 import org.genericsystem.kernel.Dependencies;
 import org.genericsystem.kernel.DependenciesImpl;
 import org.genericsystem.kernel.iterator.AbstractConcateIterator.ConcateIterator;
@@ -33,6 +34,16 @@ public class CacheDependencies<T> implements Dependencies<T> {
 
 	@Override
 	public Iterator<T> iterator() {
+		return new ConcateIterator<T>(new AbstractFilterIterator<T>(iteratorSupplier.get()) {
+			@Override
+			public boolean isSelected() {
+				return !deletes.contains(next);
+			}
+		}, inserts.iterator());
+	}
+
+	@Override
+	public Iterator<T> metaFilterIterator() {
 		return new ConcateIterator<T>(new AbstractFilterIterator<T>(iteratorSupplier.get()) {
 			@Override
 			public boolean isSelected() {
