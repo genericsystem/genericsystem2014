3a5d94f00dc7821df05d3ef2a53a6ee0faa3669c
Mohamed Khalil El Abed <kelabed@middlewarefactory.com>
2014-07-02 18:09:48 +0200

    Added tests weakEquiv method

--- gs-kernel/src/test/java/org/genericsystem/kernel/WeakEquivTest.java
+++ gs-kernel/src/test/java/org/genericsystem/kernel/WeakEquivTest.java
@@ -2,6 +2,7 @@ package org.genericsystem.kernel;
 
 import java.util.Arrays;
 
+import org.genericsystem.kernel.exceptions.ExistsException;
 import org.testng.annotations.Test;
 
 @Test
@@ -46,5 +47,40 @@ public class WeakEquivTest extends AbstractTest {
 		Vertex yellow = color.addInstance("yellow");
 		Vertex myBmwGreen = carColor.addInstance("myBmwGreen", myBmw, green);
 		assert myBmwGreen.weakEquiv(carColor, "myBmwYellow", Arrays.asList(myBmw, yellow));
+	}
+
+	public void test001_weakEquiv_Relation_SingularConstraintAndReferencialIntegrity_supers() {
+		Root engine = new Root();
+		Vertex car = engine.addInstance("Car");
+		Vertex color = engine.addInstance("Color");
+		Vertex carColor = engine.addInstance("CarColor", car, color);
+		carColor.enableSingularConstraint(1);
+		carColor.enableReferentialIntegrity(1);
+		Vertex myBmw = car.addInstance("myBmw");
+		Vertex green = color.addInstance("green");
+		Vertex yellow = color.addInstance("yellow");
+		Vertex myBmwGreen = carColor.addInstance("myBmwGreen", myBmw, green);
+		new RollbackCatcher() {
+
+			@Override
+			public void intercept() {
+				Vertex myBmwGreen2 = carColor.addInstance(myBmwGreen, "myBmwGreen2", myBmw, green);
+			}
+		}.assertIsCausedBy(ExistsException.class);
+	}
+
+	public void test001_weakEquiv_Relation_SingularConstraintAndReferencialIntegrity_setInstance() {
+		Root engine = new Root();
+		Vertex car = engine.addInstance("Car");
+		Vertex color = engine.addInstance("Color");
+		Vertex carColor = engine.addInstance("CarColor", car, color);
+		carColor.enableSingularConstraint(1);
+		carColor.enableReferentialIntegrity(1);
+		Vertex myBmw = car.addInstance("myBmw");
+		Vertex green = color.addInstance("green");
+		Vertex yellow = color.addInstance("yellow");
+		Vertex myBmwGreen = carColor.addInstance("myBmwGreen", myBmw, green);
+		Vertex myBmwGreen2 = carColor.setInstance("myBmwGreen2", myBmw, green);
+		assert !myBmwGreen.isAlive();
 	}
 }
