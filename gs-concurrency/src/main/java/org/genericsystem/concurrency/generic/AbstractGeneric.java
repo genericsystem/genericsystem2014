d8d16886edd5315668dd2a2a7ac1975b10f3aef5
mory <mory@middlewarefactory.com>
2014-07-16 12:02:34 +0200

    fix compil concurrency

---
+++ gs-concurrency/src/main/java/org/genericsystem/concurrency/generic/AbstractGeneric.java
@@ -0,0 +1,88 @@
+package org.genericsystem.concurrency.generic;
+
+import java.util.LinkedHashSet;
+
+import org.genericsystem.kernel.Snapshot;
+import org.genericsystem.kernel.Vertex;
+
+public abstract class AbstractGeneric<T extends AbstractGeneric<T>> extends org.genericsystem.cache.AbstractGeneric<T> implements GenericServiceConcurrency<T> {
+	@SuppressWarnings("unchecked")
+	@Override
+	public T plug() {
+		return getCurrentCache().plug((T) this);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean unplug() {
+		return getCurrentCache().unplug((T) this);
+	}
+
+	@Override
+	public Vertex getVertex() {
+		return super.getVertex();
+	}
+
+	@Override
+	protected Vertex unwrap() {
+		return super.unwrap();
+	}
+
+	@Override
+	protected T wrap(Vertex vertex) {
+		return super.wrap(vertex);
+	}
+
+	@Override
+	protected void forceRemove() {
+		super.forceRemove();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Snapshot<T> getInstances() {
+		return getCurrentCache().getInstances((T) this);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Snapshot<T> getInheritings() {
+		return getCurrentCache().getInheritings((T) this);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Snapshot<T> getMetaComposites(T meta) {
+		return getCurrentCache().getMetaComposites((T) this, meta);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Snapshot<T> getSuperComposites(T superVertex) {
+		return getCurrentCache().getSuperComposites((T) this, superVertex);
+	}
+
+	// @SuppressWarnings("unchecked")
+	// @Override
+	// public T getInstance(Serializable value, T... components) {
+	// T nearestMeta = adjustMeta(Collections.emptyList(), value, Arrays.asList(components));
+	// if (!equals(nearestMeta))
+	// return nearestMeta.getInstance(value, components);
+	// for (T instance : getCurrentCache().getInstances(nearestMeta))
+	// if (instance.equiv(this, value, Arrays.asList(components)))
+	// return instance;
+	// return null;
+	// }
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Snapshot<T> getComposites() {
+		return getCurrentCache().getComposites((T) this);
+	}
+
+	@Override
+	protected LinkedHashSet<T> computeDependencies() {
+		return super.computeDependencies();
+	}
+
+}
