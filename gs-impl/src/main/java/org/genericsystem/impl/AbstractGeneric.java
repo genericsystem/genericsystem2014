29603b9d572b4297f3010809d8a741e4b95c84a3
mory <mory@middlewarefactory.com>
2014-07-17 16:05:30 +0200

    merge

--- gs-impl/src/main/java/org/genericsystem/impl/AbstractGeneric.java
+++ gs-impl/src/main/java/org/genericsystem/impl/AbstractGeneric.java
@@ -33,7 +33,7 @@ public abstract class AbstractGeneric<T extends AbstractGeneric<T, U, V, W>, U e
 	@SuppressWarnings("unchecked")
 	@Override
 	public T plug() {
-		return (T) getRoot().setGenericInCache(wrap(unwrap().plug()));
+		return getRoot().getGenericFromCache(wrap(unwrap().plug()));
 	}
 
 	@Override
@@ -45,14 +45,14 @@ public abstract class AbstractGeneric<T extends AbstractGeneric<T, U, V, W>, U e
 	@SuppressWarnings("unchecked")
 	protected T wrap(V vertex) {
 		U engine = getRoot();
-		T cachedGeneric = (T) engine.getGenericFromCache(vertex);
+		T cachedGeneric = engine.getGenericFromCache(vertex);
 		if (cachedGeneric != null)
 			return cachedGeneric;
 		if (vertex.isRoot())
 			return (T) getRoot();
 		V alive = vertex.getAlive();
 		T meta = wrap(alive.getMeta());
-		return (T) getRoot().setGenericInCache(
+		return getRoot().getGenericFromCache(
 				meta.newT().init(alive.isThrowExistException(), meta, alive.getSupersStream().map(this::wrap).collect(Collectors.toList()), alive.getValue(), alive.getComponentsStream().map(this::wrap).collect(Collectors.toList())));
 	}
 
@@ -69,7 +69,7 @@ public abstract class AbstractGeneric<T extends AbstractGeneric<T, U, V, W>, U e
 	@SuppressWarnings("unchecked")
 	@Override
 	protected T bindInstance(boolean throwExistException, List<T> overrides, Serializable value, T... components) {
-		return (T) getRoot().setGenericInCache(super.bindInstance(throwExistException, overrides, value, components));
+		return getRoot().getGenericFromCache(super.bindInstance(throwExistException, overrides, value, components));
 	}
 
 	protected V getVertex() {
