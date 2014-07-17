4df65437dc8c35c3c8682569af5a935fd0b656d2
mory <mory@middlewarefactory.com>
2014-07-17 11:48:03 +0200

    fix stackoverflow : remove ovrride newT in Root

--- gs-concurrency/src/main/java/org/genericsystem/concurrency/vertex/Vertex.java
+++ gs-concurrency/src/main/java/org/genericsystem/concurrency/vertex/Vertex.java
@@ -3,13 +3,38 @@ package org.genericsystem.concurrency.vertex;
 import java.util.Iterator;
 
 import org.genericsystem.kernel.Dependencies;
+import org.genericsystem.kernel.Dependencies.DependenciesEntry;
 
 public class Vertex extends AbstractVertex<Vertex, Root> implements VertexService<Vertex, Root> {
 
-	// TODO KK FIN KK
+	private final Dependencies<Vertex> instances = buildDependencies();
+	private final Dependencies<Vertex> inheritings = buildDependencies();
+	private final Dependencies<DependenciesEntry<Vertex>> superComposites = buildDependencies();
+	private final Dependencies<DependenciesEntry<Vertex>> metaComposites = buildDependencies();
+
+	@Override
+	protected Dependencies<Vertex> getInstancesDependencies() {
+		return instances;
+	}
+
+	@Override
+	protected Dependencies<Vertex> getInheritingsDependencies() {
+		return inheritings;
+	}
+
+	@Override
+	protected Dependencies<DependenciesEntry<Vertex>> getMetaComposites() {
+		return metaComposites;
+	}
+
+	@Override
+	protected Dependencies<DependenciesEntry<Vertex>> getSuperComposites() {
+		return superComposites;
+	}
 
 	@Override
 	public Vertex newT() {
+		// TODO KK
 		Vertex vertexConcurrency = new Vertex();
 		vertexConcurrency.lifeManager = buildLifeManager();
 		return vertexConcurrency;
