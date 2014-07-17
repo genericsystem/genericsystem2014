3f6b9d8656aa7aa6128957c881bcfabe6cc3c1dd
Mohamed Khalil El Abed <kelabed@middlewarefactory.com>
2014-07-17 18:02:10 +0200

    added a test for the Cache of Generics

--- gs-impl/src/test/java/org/genercisystem/impl/GenericsCacheTest.java
+++ gs-impl/src/test/java/org/genercisystem/impl/GenericsCacheTest.java
@@ -16,4 +16,15 @@ public class GenericsCacheTest extends AbstractTest {
 		Vertex vehicleVertex = root.buildInstance(false, Collections.emptyList(), "Vehicle", Collections.emptyList());
 		assert engine.getGenericFromCache(vehicleVertex) == vehicle;
 	}
+
+	public void test002_getGenericFromCache() {
+		Engine engine = new Engine();
+		Root root = (Root) engine.getVertex();
+		Generic vehicle = engine.addInstance("Vehicle");
+		Vertex vehicleVertex = root.buildInstance(false, Collections.emptyList(), "Vehicle", Collections.emptyList());
+		assert engine.getGenericFromCache(vehicleVertex) == vehicle;
+		Generic power = engine.addInstance("Power", vehicle);
+		Vertex powerVertex = root.getMetaAttribute().buildInstance(false, Collections.emptyList(), "Power", Collections.singletonList(vehicleVertex.getAlive()));
+		assert engine.getGenericFromCache(powerVertex) == power;
+	}
 }
