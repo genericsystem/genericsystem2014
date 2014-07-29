9de22686d65692c942a7a320b3cd5d8c3a2627a8
mory <mory@middlewarefactory.com>
2014-07-29 17:03:10 +0200

    clean rollbackException

---
+++ gs-cache/src/main/java/org/genericsystem/cache/RootService.java
@@ -0,0 +1,14 @@
+package org.genericsystem.cache;
+
+import org.genericsystem.kernel.AbstractVertex;
+import org.genericsystem.kernel.services.VertexService;
+
+public interface RootService<T extends AbstractVertex<T, U>, U extends RootService<T, U>> extends org.genericsystem.kernel.services.RootService<T, U>, VertexService<T, U> {
+
+	EngineService<?, ?, T, U> getEngine();
+
+	@Override
+	default void rollback() {
+		getEngine().getCurrentCache().clear();
+	}
+}
---
+++ gs-cache/src/test/java/org/genericsystem/cache/Root.java
@@ -0,0 +1,42 @@
+package org.genericsystem.cache;
+
+import java.io.Serializable;
+import java.util.Collections;
+
+import org.genericsystem.kernel.services.ApiService;
+
+public class Root extends Vertex implements RootService<Vertex, Root> {
+
+	private final EngineService<?, ?, Vertex, Root> engine;
+
+	Root(EngineService<?, ?, Vertex, Root> engine, Serializable value) {
+		init(false, null, Collections.emptyList(), value, Collections.emptyList());
+		this.engine = engine;
+	}
+
+	@Override
+	public Root getRoot() {
+		return RootService.super.getRoot();
+	}
+
+	@Override
+	public Root getAlive() {
+		return (Root) RootService.super.getAlive();
+	}
+
+	@Override
+	public boolean equiv(ApiService<? extends ApiService<?, ?>, ?> service) {
+		return RootService.super.equiv(service);
+	}
+
+	@Override
+	public boolean isRoot() {
+		return RootService.super.isRoot();
+	}
+
+	@Override
+	public EngineService<?, ?, Vertex, Root> getEngine() {
+		return engine;
+	}
+
+}
