41d13fdd1f79ece3d420c8081f61acb4c7f06de3
nfeybesse <nfeybesse@middlewarefactory.com>
2014-07-30 11:19:06 +0200

    oops

---
+++ gs-cache/src/main/java/org/genericsystem/cache/GenericsCache.java
@@ -0,0 +1,47 @@
+package org.genericsystem.cache;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import org.genericsystem.kernel.services.ApiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenericsCache<T extends AbstractGeneric<T, ?, ?, ?>> {
+
+	private final Map<T, T> map = new ConcurrentHashMap<>();
+	static Logger log = LoggerFactory.getLogger(GenericsCache.class);
+
+	public T getOrBuildT(boolean throwExistException, T meta, List<T> supers, Serializable value, List<T> components) {
+		log.info("start find generic : " + meta + " " + value + " with supers " + supers);
+		T result = map.get(new Object() {
+			@Override
+			public int hashCode() {
+				return Objects.hashCode(value);
+			}
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public boolean equals(Object obj) {
+				if (!(obj instanceof ApiService))
+					return false;
+				return ((ApiService<T, ?>) obj).equals(meta, supers, value, components);
+			}
+		});
+		if (result == null) {
+			log.info("Not found");
+			result = meta.newT().init(throwExistException, meta, supers, value, components);
+			T alreadyPresent = map.putIfAbsent(result, result);
+			if (alreadyPresent != null) {
+				result = alreadyPresent;
+				log.info("finally found generic : " + result.info() + " with supers " + supers);
+			} else
+				log.info("Build new generic : " + result.info());
+		} else
+			log.info("found generic : " + result.info() + " with supers " + supers);
+		assert result != null;
+		return result;
+	}
+}
