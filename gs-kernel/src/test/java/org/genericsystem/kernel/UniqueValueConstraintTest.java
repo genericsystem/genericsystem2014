5a1037874708c4e9308bd794bfe8010fe637ac47
Erik Aouizerate <eaouizerate@middlewarefactory.com>
2014-10-29 14:45:06 +0100

    update uniqueConstraintTest

--- gs-kernel/src/test/java/org/genericsystem/kernel/UniqueValueConstraintTest.java
+++ gs-kernel/src/test/java/org/genericsystem/kernel/UniqueValueConstraintTest.java
@@ -19,6 +19,16 @@ public class UniqueValueConstraintTest extends AbstractTest {
 		myVehicle.disableUniqueValueConstraint();
 		assert !myVehicle.isUniqueValueEnabled();
 
+	}
+
+	public void test02_simpleCase() {
+		Root root = new Root();
+		Vertex vehicle = root.addInstance("Vehicle");
+		Vertex myVehicle = vehicle.addInstance("myVehicle");
+		Vertex myVehicle2 = vehicle.addInstance("myVehicle2");
+		Vertex power = root.addInstance("Power", vehicle);
+		myVehicle.addHolder(power, "125");
+
 		// myVehicle.enableUniqueValueConstraint();
 		//
 		// catchAndCheckCause(() -> myVehicle2.addHolder(power, "125"), UniqueValueConstraintViolationException.class);
