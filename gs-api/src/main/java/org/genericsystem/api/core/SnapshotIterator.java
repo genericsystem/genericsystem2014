b0be41c665bddc0fa60a00b9817c505d48d88d5c
Erik Aouizerate <eaouizerate@middlewarefactory.com>
2014-10-29 16:13:14 +0100

    Add SnapshotIterator

---
+++ gs-api/src/main/java/org/genericsystem/api/core/SnapshotIterator.java
@@ -0,0 +1,60 @@
+package org.genericsystem.api.core;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@FunctionalInterface
+public interface SnapshotIterator<T> {
+
+	public default Iterator<T> iterator() {
+		return get().iterator();
+
+	}
+
+	public abstract Stream<T> get();
+
+	default int size() {
+		Iterator<T> iterator = iterator();
+		int size = 0;
+		while (iterator.hasNext()) {
+			iterator.next();
+			size++;
+		}
+		return size;
+	}
+
+	default boolean isEmpty() {
+		return !iterator().hasNext();
+	}
+
+	default boolean contains(Object o) {
+		Iterator<T> it = iterator();
+		while (it.hasNext())
+			if (o.equals(it.next()))
+				return true;
+		return false;
+	}
+
+	default boolean containsAll(Collection<?> c) {
+		for (Object e : c)
+			if (!contains(e))
+				return false;
+		return true;
+	}
+
+	default T get(T o) {
+		Iterator<T> it = iterator();
+		while (it.hasNext()) {
+			T next = it.next();
+			if (o.equals(next))
+				return next;
+		}
+		return null;
+	}
+
+	default String info() {
+		return get().collect(Collectors.toList()).toString();
+	}
+}
