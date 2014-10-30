f9df4c42e7113be75040985a9e124eb1e4c0110c
nfeybesse <nfeybesse@middlewarefactory.com>
2014-10-30 09:50:30 +0100

    Little fix

--- gs-kernel/src/main/java/org/genericsystem/kernel/systemproperty/constraints/GetInstanceConstraint.java
+++ gs-kernel/src/main/java/org/genericsystem/kernel/systemproperty/constraints/GetInstanceConstraint.java
@@ -8,7 +8,7 @@ public class GetInstanceConstraint<T extends AbstractVertex<T>> implements Const
 
 	@Override
 	public void check(T modified, T attribute) throws ConstraintViolationException {
-		T generic = modified.getMeta().getInstance(modified.getValue(), modified.coerceToTArray(modified.getComposites()));
+		T generic = modified.getMeta().getInstance(modified.getValue(), modified.coerceToTArray(modified.getComposites().get().toArray()));
 		if (generic != modified)
 			throw new GetInstanceConstraintViolationException("get : " + generic.info() + " for search : " + modified.info());
 	}
