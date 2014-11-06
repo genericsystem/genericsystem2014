e9923d7029c2f49df0a385d3f37c38c2c8db0295
Erik Aouizerate <eaouizerate@middlewarefactory.com>
2014-11-06 11:10:53 +0100

    test structure for required constraint

--- gs-cache/src/test/java/org/genericsystem/cache/RequiredConstraintTest.java
+++ gs-cache/src/test/java/org/genericsystem/cache/RequiredConstraintTest.java
@@ -1,20 +1,43 @@
 package org.genericsystem.cache;
 
+import org.genericsystem.kernel.Root;
 import org.genericsystem.kernel.Statics;
+import org.genericsystem.kernel.Vertex;
 import org.testng.annotations.Test;
 
 @Test
 public class RequiredConstraintTest extends AbstractTest {
 
-	public void test001_enableRequiredConstraint_addInstance() {
-		Engine engine = new Engine();
-		Generic vehicle = engine.addInstance("Vehicle");
-		Generic color = engine.addInstance("Color");
-		Generic vehicleColor = vehicle.addAttribute("vehicleColor", color);
-		vehicleColor.enableRequiredConstraint(Statics.BASE_POSITION);
-		assert vehicleColor.isRequiredConstraintEnabled(Statics.BASE_POSITION);
-		engine.getCurrentCache().flush();
+	public void test001_enableRequired() {
+		Root engine = new Root();
+		Vertex power = engine.addInstance("Power");
+		power.enableRequiredConstraint(Statics.NO_POSITION);
+
+		assert power.isRequiredConstraintEnabled(Statics.NO_POSITION);
+		power.disableRequiredConstraint(Statics.NO_POSITION);
+		assert !power.isRequiredConstraintEnabled(Statics.NO_POSITION);
 	}
+
+	public void test002_removeAttribute() {
+		Root engine = new Root();
+		Vertex car = engine.addInstance("Car");
+		Vertex power = engine.addInstance("Power");
+		power.enableRequiredConstraint(Statics.NO_POSITION);
+
+		Vertex carPower = car.addAttribute("CarPower", power);
+		Vertex myBmw = car.addInstance("myBmw");
+
+	}
+
+	// public void test001_enableRequiredConstraint_addInstance() {
+	// Engine engine = new Engine();
+	// Generic vehicle = engine.addInstance("Vehicle");
+	// Generic color = engine.addInstance("Color");
+	// Generic vehicleColor = vehicle.addAttribute("vehicleColor", color);
+	// vehicleColor.enableRequiredConstraint(Statics.BASE_POSITION);
+	// assert vehicleColor.isRequiredConstraintEnabled(Statics.BASE_POSITION);
+	// engine.getCurrentCache().flush();
+	// }
 
 	// TODO test à décommenter et faire fonctionner
 	// public void test002_enableRequiredConstraint_addInstance() {
