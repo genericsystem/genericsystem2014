b14d36ea7cada3bd72d392b7643584ca4601c71f
nfeybesse <nfeybesse@middlewarefactory.com>
2014-11-24 11:54:44 +0100

    Merge

--- gs-kernel/src/main/java/org/genericsystem/kernel/DefaultVertex.java
+++ gs-kernel/src/main/java/org/genericsystem/kernel/DefaultVertex.java
@@ -2,12 +2,12 @@ package org.genericsystem.kernel;
 
 import java.io.Serializable;
 import java.util.stream.Stream;
-
 import org.genericsystem.api.core.IVertex;
 import org.genericsystem.api.core.Snapshot;
 
 public interface DefaultVertex<T extends AbstractVertex<T>> extends DefaultAncestors<T>, DefaultDependencies<T>, DefaultDisplay<T>, DefaultSystemProperties<T>, DefaultCompositesInheritance<T>, DefaultWritable<T>, IVertex<T> {
 
+	@Override
 	Context<T> getCurrentCache();
 
 	@Override
