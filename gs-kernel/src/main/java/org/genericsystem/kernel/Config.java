69f6e758401cffa155e883304054fdba6c55c19e
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2014-12-10 04:56:25 +0100

    oops, forget some files

---
+++ gs-kernel/src/main/java/org/genericsystem/kernel/Config.java
@@ -0,0 +1,59 @@
+package org.genericsystem.kernel;
+
+import org.genericsystem.kernel.Config.DefaultNoReferentialIntegrityProperty.DefaultValue;
+import org.genericsystem.kernel.annotations.Components;
+import org.genericsystem.kernel.annotations.Dependencies;
+import org.genericsystem.kernel.annotations.Meta;
+import org.genericsystem.kernel.annotations.Supers;
+import org.genericsystem.kernel.annotations.SystemGeneric;
+import org.genericsystem.kernel.annotations.constraints.PropertyConstraint;
+import org.genericsystem.kernel.annotations.value.AxedPropertyClassValue;
+import org.genericsystem.kernel.annotations.value.BooleanValue;
+import org.genericsystem.kernel.annotations.value.EngineValue;
+import org.genericsystem.kernel.systemproperty.NoReferentialIntegrityProperty;
+
+public class Config {
+	@SystemGeneric
+	@Meta(MetaAttribute.class)
+	@Supers(Root.class)
+	@Components(Root.class)
+	@EngineValue
+	@Dependencies({ DefaultNoReferentialIntegrityProperty.class })
+	public static class MetaAttribute extends Vertex {
+
+	}
+
+	@SystemGeneric
+	@Meta(MetaAttribute.class)
+	@Supers(SystemMap.class)
+	@Components(Root.class)
+	@AxedPropertyClassValue(propertyClass = NoReferentialIntegrityProperty.class, pos = Statics.BASE_POSITION)
+	@Dependencies({ DefaultValue.class })
+	public static class DefaultNoReferentialIntegrityProperty extends Vertex {
+
+		@SystemGeneric
+		@Meta(DefaultNoReferentialIntegrityProperty.class)
+		@Components(MetaAttribute.class)
+		@BooleanValue(true)
+		public static class DefaultValue extends Vertex {
+
+		}
+
+	}
+
+	@SystemGeneric
+	@Meta(MetaRelation.class)
+	@Supers(MetaAttribute.class)
+	@Components({ Root.class, Root.class })
+	@EngineValue
+	public static class MetaRelation extends Vertex {
+
+	}
+
+	@SystemGeneric
+	@Meta(MetaAttribute.class)
+	@Components(Root.class)
+	@PropertyConstraint
+	public static class SystemMap extends Vertex {}
+
+}
