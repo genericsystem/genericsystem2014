ff16538c7e0879d28c7caf705bb0bbb9cb573c18
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2014-12-14 19:21:15 +0100

    oops forget two files

---
+++ gs-api/src/main/java/org/genericsystem/api/exception/UnreachableOverridesException.java
@@ -0,0 +1,14 @@
+package org.genericsystem.api.exception;
+
+/**
+ * @author middleware
+ *
+ */
+public class UnreachableOverridesException extends Exception {
+
+	private static final long serialVersionUID = 6403761571054684162L;
+
+	public UnreachableOverridesException(String message) {
+		super(message);
+	}
+}
---
+++ gs-kernel/src/test/java/org/genericsystem/kernel/CornerCaseTest.java
@@ -0,0 +1,55 @@
+package org.genericsystem.kernel;
+
+import org.genericsystem.api.exception.AmbiguousSelectionException;
+import org.genericsystem.api.exception.UnreachableOverridesException;
+import org.testng.annotations.Test;
+
+@Test
+public class CornerCaseTest extends AbstractTest {
+
+	public void test000() {
+		Root engine = new Root();
+		Vertex vehicle = engine.addInstance("Vehicle");
+		Vertex car = engine.addInstance(vehicle, "Car");
+		Vertex power=vehicle.addAttribute("Power");
+		Vertex carPower = car.addAttribute(power,"CarPower");
+		Vertex carPower2 = car.addAttribute(power,"CarPower2");
+		Vertex myCar = car.addInstance("myCar");
+		catchAndCheckCause(()->myCar.addHolder(power,233),AmbiguousSelectionException.class);
+	}
+	
+	public void test001() {
+		Root engine = new Root();
+		Vertex vehicle = engine.addInstance("Vehicle");
+		Vertex car = engine.addInstance(vehicle, "Car");
+		Vertex power=vehicle.addAttribute("Power");
+		Vertex carPower = car.addAttribute(power,"CarPower");
+		Vertex myCar = car.addInstance("myCar");
+		Vertex v233 = myCar.addHolder(power,233);
+		assert v233.isInstanceOf(carPower) : v233.info();
+		Vertex v233_2 = myCar.setHolder(power,233);
+		assert v233 == v233_2;
+	}
+	
+	public void test002() {
+		Root engine = new Root();
+		Vertex vehicle = engine.addInstance("Vehicle");
+		Vertex power=vehicle.addAttribute("Power");
+		catchAndCheckCause(()->vehicle.setAttribute(power,"Power"),UnreachableOverridesException.class);
+		
+	}
+	
+	public void test003() {
+		Root engine = new Root();
+		Vertex vehicle = engine.addInstance("Vehicle");
+		Vertex car = engine.addInstance(vehicle, "Car");
+		Vertex power = vehicle.addAttribute("Power");
+
+		Vertex myBmw = car.addInstance("myBmw");
+		Vertex holder = myBmw.addHolder(power, 235);
+		assert holder.getMeta().equals(power);
+		Vertex carPower = car.addAttribute(power, "CarPower");
+		assert carPower.equals(myBmw.getHolders(power).first().getMeta());
+	}
+
+}
