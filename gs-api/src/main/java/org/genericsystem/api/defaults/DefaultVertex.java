7619c16638d8dc9b3c24bc6e9ce2ed1748d3790a
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2015-01-14 14:47:59 +0100

    Fix singular issue in isSuperOf

--- gs-api/src/main/java/org/genericsystem/api/defaults/DefaultVertex.java
+++ gs-api/src/main/java/org/genericsystem/api/defaults/DefaultVertex.java
@@ -36,8 +36,12 @@ public interface DefaultVertex<T extends DefaultVertex<T>> extends DefaultAncest
 				return false;
 		} else if (!subMeta.inheritsFrom(superMeta))
 			return false;
-		if (!superMeta.componentsDepends(subComponents, superComponents))
+		boolean[] singular = new boolean[1];
+		if (!superMeta.componentsDepends(subComponents, superComponents, singular))
 			return false;
+		if (singular[0])
+			return true;
+
 		if (superMeta.isPropertyConstraintEnabled())
 			if (!subComponents.equals(superComponents))
 				return true;
@@ -58,21 +62,26 @@ public interface DefaultVertex<T extends DefaultVertex<T>> extends DefaultAncest
 		return overrides.stream().anyMatch(override -> override.inheritsFrom((T) this)) || isSuperOf(subMeta, subValue, subComponents, getMeta(), getValue(), getComponents());
 	}
 
-	@SuppressWarnings("unchecked")
 	default boolean componentsDepends(List<T> subComponents, List<T> superComponents) {
+		return componentsDepends(subComponents, superComponents, new boolean[1]);
+	}
+
+	@SuppressWarnings("unchecked")
+	default boolean componentsDepends(List<T> subComponents, List<T> superComponents, boolean[] singular) {
+		singular[0] = false;
 		int subIndex = 0;
 		loop: for (T superComponent : superComponents) {
 			for (; subIndex < subComponents.size(); subIndex++) {
 				T subComponent = subComponents.get(subIndex);
 				if ((subComponent == null && superComponent == null) || (subComponent != null && superComponent != null && subComponent.isSpecializationOf(superComponent))
 						|| (subComponent == null && superComponent != null && this.isSpecializationOf(superComponent)) || (subComponent != null && superComponent == null && subComponent.isSpecializationOf((T) this))) {
-					if (isSingularConstraintEnabled(subIndex))
-						return true;
+					if (isSingularConstraintEnabled(subIndex) && subComponent != superComponent)
+						singular[0] = true;
 					subIndex++;
 					continue loop;
 				}
 			}
-			return false;
+			return singular[0];
 		}
 		return true;
 	}
