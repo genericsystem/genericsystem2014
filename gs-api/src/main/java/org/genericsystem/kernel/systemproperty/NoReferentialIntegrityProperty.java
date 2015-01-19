67d4b321c8584ccc06cf0558e91fb575871035b3
Sylvain Collet <scollet@middlewarefactory.com>
2015-01-19 14:16:20 +0100

    API documentation of org.genericsystem.kernel.systemproperty package.

--- gs-api/src/main/java/org/genericsystem/kernel/systemproperty/NoReferentialIntegrityProperty.java
+++ gs-api/src/main/java/org/genericsystem/kernel/systemproperty/NoReferentialIntegrityProperty.java
@@ -2,6 +2,11 @@ package org.genericsystem.kernel.systemproperty;
 
 import org.genericsystem.api.core.IVertex.SystemProperty;
 
+/**
+ * The System Property indicating <em>NO</em> referential integrity.
+ * 
+ * @author Nicolas Feybesse
+ */
 public class NoReferentialIntegrityProperty implements SystemProperty {
 
 }
