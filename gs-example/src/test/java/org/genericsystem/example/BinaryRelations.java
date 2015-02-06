ec6752d68ac6c831f2a5a8ff22b7b52bd617ea4f
Sylvain Collet <scollet@middlewarefactory.com>
2015-02-06 18:02:18 +0100

    Add case of ternary relation in creation of relations and links example. Add case of many to many relation modified and read from the base as the target in binary relations example.

--- gs-example/src/test/java/org/genericsystem/example/BinaryRelations.java
+++ gs-example/src/test/java/org/genericsystem/example/BinaryRelations.java
@@ -1,6 +1,9 @@
 package org.genericsystem.example;
 
+import java.util.Arrays;
+
 import org.genericsystem.api.core.ApiStatics;
+import org.genericsystem.api.core.Snapshot;
 import org.genericsystem.mutability.Engine;
 import org.genericsystem.mutability.Generic;
 
@@ -137,6 +140,76 @@ public class BinaryRelations {
 
 		// Create the link between yourVehicle and red from the relation VehicleColor
 		yourVehicle.addLink(vehicleColor, "yourVehicleRed", red); // OK
+
+		// Persist changes
+		engine.getCurrentCache().flush();
+	}
+
+	public void manyToManyRelationAndSnapshot() {
+		Engine engine = new Engine();
+
+		Generic vehicle = engine.addInstance("Vehicle");
+		Generic color = engine.addInstance("Color");
+		Generic vehicleColor = vehicle.addRelation("VehicleColor", color);
+
+		// Make VehicleColor a n-m relation : nothing to do, it is the case by default
+
+		Generic myVehicle = vehicle.addInstance("myVehicle");
+		Generic yourVehicle = vehicle.addInstance("yourVehicle");
+
+		Generic red = color.addInstance("red");
+		Generic yellow = color.addInstance("yellow");
+
+		// Create links from the base
+		Generic myVehicleYellow = myVehicle.addLink(vehicleColor, "myVehicleYellow", yellow);
+
+		// Create links from the target
+		// Generic yourVehicleRed = red.addLink(vehicleColor, "yourVehicleRed", yourVehicle);
+		Generic yourVehicleRed = yourVehicle.addLink(vehicleColor, "yourVehicleRed", red);
+
+		// Find links from the base
+		Snapshot<Generic> linksFromTheBase = myVehicle.getLinks(vehicleColor);
+		assert linksFromTheBase.size() == 1;
+		assert linksFromTheBase.containsAll(Arrays.asList(myVehicleYellow));
+
+		// Find links from the target
+		Snapshot<Generic> linksFromTheTarget = red.getLinks(vehicleColor);
+		assert linksFromTheTarget.size() == 1;
+		assert linksFromTheTarget.containsAll(Arrays.asList(yourVehicleRed));
+
+		// Add a link from the base
+		Generic myVehicleRed = myVehicle.addLink(vehicleColor, "myVehicleRed", red);
+
+		// Links from the base have been modified and are aware of these changes...
+		assert linksFromTheBase.size() == 2;
+		assert linksFromTheBase.containsAll(Arrays.asList(myVehicleYellow, myVehicleRed));
+
+		// ...but links from the target have been modified too and are aware of these changes too
+		assert linksFromTheTarget.size() == 2;
+		assert linksFromTheTarget.containsAll(Arrays.asList(yourVehicleRed, myVehicleRed));
+
+		// Test the same thing but in the other side
+		// Remove the last link
+		myVehicleRed.remove();
+		assert !myVehicleRed.isAlive();
+
+		assert linksFromTheBase.size() == 1;
+		assert linksFromTheBase.containsAll(Arrays.asList(myVehicleYellow));
+
+		assert linksFromTheTarget.size() == 1;
+		assert linksFromTheTarget.containsAll(Arrays.asList(yourVehicleRed));
+
+		// Add a link from the target
+		// myVehicleRed = red.addLink(vehicleColor, "myVehicleRed", myVehicle);
+		myVehicleRed = myVehicle.addLink(vehicleColor, "myVehicleRed", red);
+
+		// Links from the target have been modified and are aware of these changes...
+		assert linksFromTheTarget.size() == 2;
+		assert linksFromTheTarget.containsAll(Arrays.asList(yourVehicleRed, myVehicleRed));
+
+		// ...but links from the base have been modified too and are aware of these changes too
+		assert linksFromTheBase.size() == 2;
+		assert linksFromTheBase.containsAll(Arrays.asList(myVehicleYellow, myVehicleRed));
 
 		// Persist changes
 		engine.getCurrentCache().flush();
--- gs-example/src/test/java/org/genericsystem/example/CreationOfRelationsAndLinks.java
+++ gs-example/src/test/java/org/genericsystem/example/CreationOfRelationsAndLinks.java
@@ -38,4 +38,27 @@ public class CreationOfRelationsAndLinks {
 		// Persist changes
 		engine.getCurrentCache().flush();
 	}
+
+	public void createTernaryRelation() {
+		Engine engine = new Engine();
+
+		// Create the types Vehicle, Color and Time
+		Generic vehicle = engine.addInstance("Vehicle");
+		Generic color = engine.addInstance("Color");
+		Generic time = engine.addInstance("Time");
+
+		// Create the relation VehicleColorTime between Vehicle, Color and Time
+		Generic vehicleColorTime = vehicle.addRelation("VehicleColorTime", color, time);
+
+		// Create an instance of type Vehicle, an instance of type Color and an instance of type Time
+		Generic myVehicle = vehicle.addInstance("myVehicle");
+		Generic red = color.addInstance("red");
+		Generic now = time.addInstance("now");
+
+		// Do the link between the instance of the vehicle, the instance of the color and the instance of the time
+		myVehicle.addLink(vehicleColorTime, "myVehicleRedNow", red, now);
+
+		// Persist changes
+		engine.getCurrentCache().flush();
+	}
 }
