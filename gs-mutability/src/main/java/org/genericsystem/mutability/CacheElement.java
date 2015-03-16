d92bf001488aa5597f02997eb9ca47e22c62d23d
Vincent Ruch <vruch@middlewarefactory.com>
2015-03-16 15:31:38 +0100

    change implementation of cache element : change revertMutation map from inheritance to composition

--- gs-mutability/src/main/java/org/genericsystem/mutability/CacheElement.java
+++ gs-mutability/src/main/java/org/genericsystem/mutability/CacheElement.java
@@ -1,11 +1,13 @@
 package org.genericsystem.mutability;
 
 import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
-public class CacheElement extends IdentityHashMap<Generic, org.genericsystem.kernel.Generic> {
+public class CacheElement {
 
-	private static final long serialVersionUID = 776238941633932479L;
 	private final CacheElement subCache;
+	private final Map<Generic, org.genericsystem.kernel.Generic> revertMutations = new IdentityHashMap<>();
 
 	public CacheElement() {
 		this(null);
@@ -18,5 +20,13 @@ public class CacheElement extends IdentityHashMap<Generic, org.genericsystem.ker
 
 	public CacheElement getSubCache() {
 		return subCache;
+	}
+
+	void putIfAbsent(Generic key, org.genericsystem.kernel.Generic value) {
+		revertMutations.putIfAbsent(key, value);
+	}
+
+	void forEach(BiConsumer<? super Generic, ? super org.genericsystem.kernel.Generic> action) {
+		revertMutations.forEach(action);
 	}
 }
