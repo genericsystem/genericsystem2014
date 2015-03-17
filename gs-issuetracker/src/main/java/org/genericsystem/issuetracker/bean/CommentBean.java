a9b5c7b8e6d69c68158cd230c228fd6e7ff665a3
Ingvild Gaulin <igaulin@middlewarefactory.com>
2015-03-17 12:23:50 +0100

    issuetracker : update for comments

--- gs-issuetracker/src/main/java/org/genericsystem/issuetracker/bean/CommentBean.java
+++ gs-issuetracker/src/main/java/org/genericsystem/issuetracker/bean/CommentBean.java
@@ -2,13 +2,17 @@ package org.genericsystem.issuetracker.bean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import org.genericsystem.issuetracker.bean.IssueBean.ElStringWrapper;
 import org.genericsystem.issuetracker.model.Comment;
+import org.genericsystem.issuetracker.model.Issue;
 import org.genericsystem.issuetracker.model.IssueComment;
 import org.genericsystem.issuetracker.qualifier.Provide;
 import org.genericsystem.mutability.Generic;
@@ -20,37 +24,46 @@ public class CommentBean {
 
 	@Inject
 	@Provide
+	private Issue issue;
+
+	@Inject
+	@Provide
 	private IssueComment issueComment;
 
 	@Inject
 	@Provide
 	private Comment comment;
 
-	@Inject
-	private IssueSelectionBean issueSelectionBean;
-
-	private String newComment;
-
 	private List<Generic> getCommentsByIssue(Generic instance) {
 		return instance.getLinks(issueComment).get().collect(Collectors.toList());
 	}
 
-	public List<Generic> getComments() {
-		Generic instance = issueSelectionBean.getSelectedIssue();
+	public List<Generic> getComments(Generic instance) {
 		return (instance != null) ? getCommentsByIssue(instance) : new ArrayList<>();
 	}
 
-	public ElStringWrapper addComment(Generic instance) {
+	public ElStringWrapper getComment(Generic instance, Generic selectedComment) {
 		return new ElStringWrapper() {
 
 			@Override
 			public void setValue(String value) {
+				log.info("CommentBean ; getComment ; setValue ; value : " + value);
 				instance.setLink(issueComment, "link", comment.setInstance(value));
+
+				// TODO update value of selectedComment
+				selectedComment.getTargetComponent().updateValue(value);
+				log.info("CommentBean ; getComment ; setValue ; selectedComment : " + selectedComment.info());
 			}
 
 			@Override
 			public String getValue() {
-				return "";
+				String returnString = null;
+				if (selectedComment != null) {
+					log.info("CommentBean ; getComment ; getValue ; selectedComment.getTargetComponent : " + selectedComment.getTargetComponent().info());
+					returnString = Objects.toString(selectedComment.getTargetComponent().getValue());
+				}
+				log.info("CommentBean ; getComment ; getValue ; returnString : " + returnString);
+				return (returnString != null) ? returnString : "";
 			}
 		};
 	}
@@ -58,15 +71,6 @@ public class CommentBean {
 	public String deleteComment(Generic comment) {
 		comment.remove();
 		return "#";
-	}
-
-	public String getNewComment() {
-		return newComment;
-	}
-
-	public void setNewComment(String newComment) {
-		log.info("CommentBean ; setNewComment ; commentValue : " + newComment);
-		this.newComment = newComment;
 	}
 
 }
