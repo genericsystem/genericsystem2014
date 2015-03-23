737bdb5e8964a6826a8b1435f93d93193ae029f0
mory <mory@middlewarefactory.com>
2015-03-23 14:31:34 +0100

    add test for CascadeRemove

--- gs-kernel/src/test/java/org/genericsystem/kernel/CascadeRemoveTest.java
+++ gs-kernel/src/test/java/org/genericsystem/kernel/CascadeRemoveTest.java
@@ -1,25 +1,92 @@
 package org.genericsystem.kernel;
 
 import org.genericsystem.api.core.ApiStatics;
+import org.genericsystem.api.core.exceptions.ReferentialIntegrityConstraintViolationException;
 import org.testng.annotations.Test;
 
 @Test
 public class CascadeRemoveTest extends AbstractTest {
 
-	public void testWhithCascadeRemove() {
+	public void test001_CascadeRemove() {
 		Root root = new Root();
 		Generic car = root.addInstance("Car");
 		Generic color = root.addInstance("Color");
-		Generic carColor = car.addInstance("CarColor", color);
+		Generic carColor = car.addRelation("CarColor", color);
+		carColor.enableCascadeRemove(ApiStatics.BASE_POSITION);
+		Generic myCar = car.addInstance("myCar");
+		Generic red = color.addInstance("red");
+		Generic myCarRed = myCar.addLink(carColor, "defaultColor", red);
+		myCarRed.remove();
+		assert red.isAlive();
+		assert !myCarRed.isAlive();
+		assert !myCarRed.getBaseComponent().isAlive();
+	}
+
+	public void test001_CascadeRemove_TargetPosition() {
+		Root root = new Root();
+		Generic car = root.addInstance("Car");
+		Generic color = root.addInstance("Color");
+		Generic carColor = car.addRelation("CarColor", color);
 		carColor.enableCascadeRemove(ApiStatics.TARGET_POSITION);
 		Generic myCar = car.addInstance("myCar");
-		Generic link = myCar.addLink(carColor, "red", color);
+		Generic red = color.addInstance("red");
+		Generic myCarRed = myCar.addLink(carColor, "defaultColor", red);
+		myCarRed.remove();
 		assert myCar.isAlive();
-		assert link.isAlive();
-		assert link.getTargetComponent().isAlive();
-		link.remove();
+		assert !myCarRed.isAlive();
+		assert !myCarRed.getTargetComponent().isAlive();
+	}
+
+	public void test002_CascadeRemove() {
+		Root root = new Root();
+		Generic car = root.addInstance("Car");
+		Generic color = root.addInstance("Color");
+		Generic carColor = car.addRelation("CarColor", color);
+		carColor.enableCascadeRemove(ApiStatics.BASE_POSITION);
+		Generic myCar = car.addInstance("myCar");
+		Generic myCarColor = myCar.addLink(carColor, "defaultColor", color);
+		myCarColor.remove();
+		assert color.isAlive();
+		assert !myCarColor.isAlive();
+		assert !myCarColor.getBaseComponent().isAlive();
+	}
+
+	public void test002_CascadeRemove_TargetPosition() {
+		Root root = new Root();
+		Generic car = root.addInstance("Car");
+		Generic color = root.addInstance("Color");
+		Generic carColor = car.addRelation("CarColor", color);
+		carColor.enableCascadeRemove(ApiStatics.TARGET_POSITION);
+		Generic red = color.addInstance("red");
+		Generic carRed = car.addLink(carColor, "defaultColor", red);
+		carRed.remove();
+		assert car.isAlive();
+		assert !carRed.isAlive();
+		assert !carRed.getTargetComponent().isAlive();
+	}
+
+	public void test003_CascadeRemove() {
+		Root root = new Root();
+		Generic car = root.addInstance("Car");
+		Generic color = root.addInstance("Color");
+		Generic carColor = car.addRelation("CarColor", color);
+		carColor.enableCascadeRemove(ApiStatics.BASE_POSITION);
+		Generic red = color.addInstance("red");
+		Generic carRed = car.addLink(carColor, "defaultColor", red);
+		catchAndCheckCause(() -> carRed.remove(), ReferentialIntegrityConstraintViolationException.class);
+	}
+
+	public void test003_CascadeRemove_TargetPosition() {
+		Root root = new Root();
+		Generic car = root.addInstance("Car");
+		Generic color = root.addInstance("Color");
+		Generic carColor = car.addRelation("CarColor", color);
+		carColor.enableCascadeRemove(ApiStatics.TARGET_POSITION);
+		Generic myCar = car.addInstance("myCar");
+		Generic myCarColor = myCar.addLink(carColor, "defaultColor", color);
 		assert myCar.isAlive();
-		assert !link.isAlive();
-		assert !link.getTargetComponent().isAlive();
+		assert myCarColor.isAlive();
+		assert myCarColor.getTargetComponent().isAlive();
+		catchAndCheckCause(() -> myCarColor.remove(), ReferentialIntegrityConstraintViolationException.class);
 	}
 }
